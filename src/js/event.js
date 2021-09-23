10728ae57122093c54e33842818a0f839db5903d
eleftheros <eleftheros_all@protonmail.com>
2021-09-23 13:39:51 +0300

    Fix reset method

--- src/js/event.js
+++ src/js/event.js
@@ -24,7 +24,7 @@ class Event {
     this.startTimer();
   }
   reset() {
-    this.setEventTime = null;
+    this.eventTime = null;
     clearInterval(this.timer);
     ui.updateTheme(this.events, null);
   }
